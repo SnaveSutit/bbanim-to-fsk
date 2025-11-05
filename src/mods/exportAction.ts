6540c08980213ec5d384384487b45365a98264ba
shadow00dev <twistcharlievlogs@gmail.com>
2025-11-05 17:22:46 +0000

    Added translate support

--- src/mods/exportAction.ts
+++ src/mods/exportAction.ts
@@ -2,6 +2,37 @@ import PACKAGE from '../../package.json'
 import { renderAnimationAsFSK } from '../animationRenderer'
 import { createAction } from '../util/moddingTools'
 
+function convertPlayerCubes(input: string) {
+	const lines: any = input
+		.split(/\r?\n|;/)
+		.map(l => l.trim())
+		.filter(Boolean)
+	const axes: any = { X: [1, 0, 0], Y: [0, 1, 0], Z: [0, 0, 1] }
+	return lines
+		.map((line: any) => {
+			const match = line.match(
+				/\{(Player_(rot|pos)([XYZ]))\}\s*\+=\s*([+-]?\d+(?:\.\d+)?)'?(\{\d+\})?/
+			)
+			if (!match) return line
+			const [, , type, axis, value, index = ''] = match
+			let num = parseFloat(value)
+			if (axis === 'X' || axis === 'Y') {
+				num = -num
+			}
+			if (type === 'rot') {
+				return `out "rotate" (${num}${index}, ${axes[axis].join(', ')});${line}`
+			}
+			if (type === 'pos') {
+				const conv = num / 16
+				return `out "translate" (${axes[axis]
+					.map((v: any) => (v ? `${conv}${index}` : 0))
+					.join(', ')});${line}`
+			}
+			return line
+		})
+		.join('\n')
+}
+
 const condition = () => {
 	return !!Project?.format.animation_mode
 }
@@ -16,7 +47,7 @@ const EXPORT_ACTION = createAction(`${PACKAGE.name}:export`, {
 			EXPORT_AS_ACTION.click()
 			return
 		}
-		const content = renderAnimationAsFSK(Animator.selected!)
+		const content = convertPlayerCubes(renderAnimationAsFSK(Animator.selected!))
 		Blockbench.writeFile(Project?.lastExportPath || '', {
 			content,
 			savetype: 'text',
@@ -34,7 +65,7 @@ const EXPORT_AS_ACTION = createAction(`${PACKAGE.name}:exportAsAction`, {
 		const file: string[] = []
 		for (const animation of Project!.animations) {
 			if (!animation.export) continue
-			const content = renderAnimationAsFSK(animation)
+			const content = convertPlayerCubes(renderAnimationAsFSK(animation))
 			file.push(content)
 		}
 		Blockbench.export(
